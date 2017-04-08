7af75cacd007d26215dfb4fa28e2435769ab8915
Guaifan <zousitanghulu@foxmail.com>
2017-04-08 13:38:16 +0800

    添加项目

---
+++ quansheng/src/main/web/src/Plugin/js/index.js
@@ -0,0 +1,27 @@
+let $ = require('jquery');
+
+$("#next").click(function () {
+    let firstShow = $(".show").eq(0);
+
+    let left =$("#scroll-div").css("left");
+    left=left.replace("px","");
+    if(left>-300){
+        left=left-120;
+        console.log(left);
+        $("#scroll-div").animate({left: left+'px'});
+    }
+
+});
+
+$("#prev").click(function () {
+    let firstShow = $(".show").eq(0);
+
+    let left =$("#scroll-div").css("left");
+    left=left.replace("px","");
+    if(left<0){
+        left=left-0+120;
+        console.log(left);
+        $("#scroll-div").animate({left: left+'px'});
+    }
+
+});

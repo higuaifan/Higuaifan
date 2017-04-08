49036927fab1b96d06413a3ddd3732176f312c1d
Higuaifan <zousitanghulu@foxmail.com>
2017-04-08 20:57:47 +0800

    Revert "update taobao.py"
    This reverts commit 1c07ec8ed706a8efb1e9c6d9aea044dc387413dc.

--- xdd/python/taobao.py
+++ xdd/python/taobao.py
@@ -47,17 +47,13 @@ def get(json_data):
     for i in item:
         base_info = i['baseinfo']
         i_name = i['name']
-        price_arr = i['price']
-        price = price_arr['actPrice']
         image = base_info['picUrl']
         item_url = base_info['itemUrl']
         print image
         print item_url
         short_name = i_name['shortName']
         print short_name
-        print price
-        
-        # insert(short_name, image, item_url)
+        insert(short_name, image, item_url)
 
 
 fh_get_type_and_link('https://ju.taobao.com/')

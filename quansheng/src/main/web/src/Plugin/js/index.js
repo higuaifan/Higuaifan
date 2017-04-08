45d5a68af3f2b91726cdbbd75990096cff1596b7
Guaifan <zousitanghulu@foxmail.com>
2017-04-09 02:13:21 +0800

    update quansheng

--- quansheng/src/main/web/src/Plugin/js/index.js
+++ quansheng/src/main/web/src/Plugin/js/index.js
@@ -1,14 +1,14 @@
-let $ = require('jquery');
+
 
 $("#next").click(function () {
     let firstShow = $(".show").eq(0);
 
-    let left =$("#scroll-div").css("left");
-    left=left.replace("px","");
-    if(left>-300){
-        left=left-120;
+    let left = $("#scroll-div").css("left");
+    left = left.replace("px", "");
+    if (left > -300) {
+        left = left - 120;
         console.log(left);
-        $("#scroll-div").animate({left: left+'px'});
+        $("#scroll-div").animate({left: left + 'px'});
     }
 
 });
@@ -16,12 +16,21 @@ $("#next").click(function () {
 $("#prev").click(function () {
     let firstShow = $(".show").eq(0);
 
-    let left =$("#scroll-div").css("left");
-    left=left.replace("px","");
-    if(left<0){
-        left=left-0+120;
+    let left = $("#scroll-div").css("left");
+    left = left.replace("px", "");
+    if (left < 0) {
+        left = left - 0 + 120;
         console.log(left);
-        $("#scroll-div").animate({left: left+'px'});
+        $("#scroll-div").animate({left: left + 'px'});
     }
 
 });
+
+(function($){
+    $(window).load(function(){
+        $("#my-thumbs-list").mThumbnailScroller({
+            axis:"x", //change to "y" for vertical scroller
+            type:"click-thumb"
+        });
+    });
+})(jQuery);

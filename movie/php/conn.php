fcefecc46008df5e3251becb7666b252753d3c91
你好怪凡 <higuaifan@higuaifan.com>
2023-02-13 09:58:25 +0800

    remove all files

--- movie/php/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$conn = new mysqli("rm-wz9h86151kd3mlx1io.mysql.rds.aliyuncs.com", "zxmysql", "ZX123456zx!", "spider");
-// $conn=new mysqli("localhost:3306","root","root","JiaJiaFood"); 
-mysqli_query($conn, "SET NAMES utf8");
-if (mysqli_connect_errno($conn))
-    die("连接 MySQL 失败: " . mysqli_connect_error());
